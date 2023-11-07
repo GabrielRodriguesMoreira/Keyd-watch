a0f507a3c89c6748ffb68d32d24e20402e5c7e12
GabrielRodriguesMoreira <gabriel.rodrigues.moreira08@aluno.ifce.edu.br>
2023-11-06 21:33:32 -0300

    init

--- src/app/componenets/disqus.jsx
+++ src/app/componenets/disqus.jsx
@@ -2,8 +2,20 @@
 import React, { Component } from 'react';
 
 class DisqusComments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showRefreshButton: false,
+    };
+  }
+
   componentDidMount() {
     this.loadDisqus();
+    this.setRefreshInterval();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.refreshInterval);
   }
 
   loadDisqus() {
@@ -17,22 +29,48 @@ class DisqusComments extends Component {
       window.DISQUS.reset({
         reload: true,
         config: function () {
-          this.page.url = 'localhost'; // Replace with your page's URL
-          this.page.identifier = 'localhost'; // Replace with your page's unique identifier
+          this.page.url = 'https://keyd-watch.vercel.app/'; // Replace with your page's URL
+          this.page.identifier = 'https://keyd-watch.vercel.app/'; // Replace with your page's unique identifier
         },
       });
     }
   }
 
+  setRefreshInterval() {
+    this.refreshInterval = setInterval(() => {
+      this.setState({ showRefreshButton: true });
+    }, 3 * 60 * 1000); // 3 minutes in milliseconds
+  }
+
+  handleRefreshClick = () => {
+    this.loadDisqus();
+    this.setState({ showRefreshButton: false });
+  };
+
+
   render() {
     return (
-      <>
-        <div id="disqus_thread" className='h-full w-full'></div>
+      <div className='w-full h-full relative '>
+        <div id="disqus_thread" className='h-full w-full pr-3'></div>
+        <button
+            className="fixed bottom-[60%] right-[5%] transform -translate-x-1/2 bg-purple-800 text-white p-2 rounded-full text-sm font-semibold shadow-lg shadow-black animate-pulse slide-in"
+            onClick={this.handleRefreshClick}
+          >
+            Load new posts
+          </button>
+        {this.state.showRefreshButton && (
+          <button
+            className="absolute top-5 left-[50%] transform -translate-x-1/2 bg-purple-800 text-white p-2 rounded-full text-sm font-semibold shadow-lg shadow-black animate-pulse slide-in"
+            onClick={this.handleRefreshClick}
+          >
+            Load new posts
+          </button>
+        )}
         <noscript>
           Please enable JavaScript to view the{' '}
           <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
         </noscript>
-      </>
+      </div>
     );
   }
 }
