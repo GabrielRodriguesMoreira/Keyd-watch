a3ff9ba346b7f0ab9e48273a135c3ba84f6d5551
GabrielRodriguesMoreira <gabriel.rodrigues.moreira08@aluno.ifce.edu.br>
2023-11-11 23:05:16 -0300

    init

--- src/app/live/page.jsx
+++ src/app/live/page.jsx
@@ -19,7 +19,12 @@ import DisqusComments from '../componenets/disqus'
 
 export default function Live() {
 
-    const [swapScreen, setSwapScreen] = useState(false)
+    const [swapScreen, setSwapScreen] = useState({
+        ytScreen: ' col-start-1 row-start-1 col-span-9 row-span-9',
+        twScreen: ' col-start-10 row-start-1 col-span-3 row-span-3',
+        third: '',
+        controls: ' col-span-9',
+    })
     const [player, setPlayer] = useState(null);
     const [isPlaying, setIsPlaying] = useState(false);
     const [isMobile, setIsMobile] = useState(0);
@@ -33,11 +38,11 @@ export default function Live() {
         },
     };
 
-    useEffect(()=>{
-        if(window.innerWidth <= 700){
+    useEffect(() => {
+        if (window.innerWidth <= 700) {
             setIsMobile(1)
         };
-    },[])
+    }, [])
 
     const onReady = (event) => {
         setPlayer(event.target);
@@ -91,17 +96,44 @@ export default function Live() {
         }
     };
 
+    const swapScreenfunc = (set) => {
+        switch (set) {
+            case 1: setSwapScreen({
+                ytScreen: ' col-start-1 row-start-1 col-span-9 row-span-9',
+                twScreen: ' col-start-10 row-start-1 col-span-3 row-span-3',
+                third: '',
+                controls: ' col-span-9',
+            })
+                break
+            case 2: setSwapScreen({
+                ytScreen: ' col-start-10 row-start-1 col-span-3 row-span-3',
+                twScreen: ' col-start-1 row-start-1 col-span-9 row-span-9',
+                third: '',
+                controls: ' col-span-9',
+            })
+                break
+            case 3:
+                setSwapScreen({
+                    ytScreen: ' col-start-1 row-start-1 col-span-6 row-span-6',
+                    twScreen: ' col-start-7 row-start-1 col-span-6 row-span-6 ',
+                    third: ' col-start-7 row-start-7 row-span-3 col-span-6',
+                    controls: ' row-start-7 col-start-1 col-span-6 row-span-3',
+                })
+                break
+        }
+
+    }
 
     return (
         <main className="w-full flex flex-wrap justify-center lg:p-2 lg:gap-2 lg:h-screen lg:grid lg:grid-cols-12 lg:grid-rows-10 lg:pl-4">
 
             {/* Live do Youtube */}
-            <div className={`w-full max-w-sm lg:max-w-none ${swapScreen ? "col-start-10 row-start-1 col-span-3 row-span-3" : "col-start-1 row-start-1 col-span-9 row-span-9"} lg:rounded-md `} >
+            <div className={"w-full max-w-sm lg:max-w-none lg:rounded-md" + swapScreen.ytScreen} >
                 {
                     liveId ?
                         <div className='w-full relative lg:rounded-md aspect-video overflow-hidden'>
-                            <div className='absolute top-0 left-0 w-full h-full pointer-events-none' style={{ boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.8)' }}></div>
-                            <YouTube iframeClassName='w-full h-full aspect-video' videoId={liveId} opts={opts} onReady={onReady} />
+                            <div className='absolute top-0 left-0 w-full h-full pointer-events-none aspect-video' style={{ boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.8)' }}></div>
+                            <YouTube iframeClassName='w-full h-full aspect-video -z-10' videoId={liveId} opts={opts} onReady={onReady} />
                         </div>
                         :
                         <div className="w-full relative flex flex-col items-center aspect-video text-black p-3 pb-0" >
@@ -112,9 +144,9 @@ export default function Live() {
             </div>
 
             {/* Controles */}
-            <div className=" w-full items-start row-start-10 col-span-9 text-white  justify-between text-xl  hidden lg:flex" >
+            <div className={" w-full items-start row-start-10  text-white  justify-between text-xl  hidden lg:flex" + swapScreen.controls} >
 
-                <div className='flex w- justify-center items-center space-x-1 cursor-pointer order-1 w-36 overflow-hidden group'>
+                <div className='flex  justify-center items-center space-x-1 cursor-pointer order-1 w-36 overflow-hidden group'>
                     <button onClick={() => { document.getElementById("range_input").value = 0 }}>
                         <IconWithTooltip icon={<MdVolumeUp />} text="volume" />
                     </button>
@@ -155,23 +187,39 @@ export default function Live() {
                     <button onClick={toggleSubTitle}>
                         <IconWithTooltip icon={<MdOutlineSubtitles />} text="Subtitles" />
                     </button>
-                    <button onClick={() => setSwapScreen(!swapScreen)}>
-                        <IconWithTooltip icon={<MdSwapHoriz />} text="Swap Screens" />
+                    <button onClick={() => { swapScreenfunc(3) }}>
+                        <IconWithTooltip icon={<svg width="26" height="16" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="1" y="1" width="10" height="10" stroke="red" fill="none" />
+                            <rect x="15" y="1" width="10" height="10" stroke="purple" fill="none" />
+                        </svg>} text="Side by side" />
+                    </button>
+                    <button onClick={() => { swapScreenfunc(1) }}>
+                        <IconWithTooltip icon={<svg width="26" height="16" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="1" y="1" width="10" height="10" stroke="red" fill="none" />
+                            <rect x="15" y="1" width="6" height="6" stroke="purple" fill="none" />
+                        </svg>} text="big youtube" />
+                    </button>
+                    <button onClick={() => { swapScreenfunc(2) }}>
+                        <IconWithTooltip icon={<svg width="26" height="16" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="1" y="1" width="10" height="10" stroke="purple" fill="none" />
+                            <rect x="15" y="1" width="6" height="6" stroke="red" fill="none" />
+                        </svg>} text="big twitch" />
                     </button>
                 </div>
             </div>
 
             {/* Live da Twitch */}
-            <div className={` w-full max-w-sm lg:max-w-none overflow-hidden lg:rounded-md ${swapScreen ? "col-start-1 row-start-1 col-span-9 row-span-9" : "col-start-10 row-start-1 col-span-3 row-span-3"}`}>
+            <div className={"w-full relative max-w-sm lg:max-w-none overflow-hidden lg:rounded-md" + swapScreen.twScreen}>
+                <div className='absolute top-0 left-0 w-full pointer-events-none aspect-video' style={{ boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.8)' }}></div>
                 <iframe
-                    className="w-full aspect-video relative lg:rounded-md"
-                    src="https://player.twitch.tv/?channel=patopapao&amp;enableExtensions=false&amp;parent=twitch.tv&amp;quality=auto&amp;volume=1&amp;parent=keyd-watch.vercel.app" type="text/html" allowFullScreen
+                    className="w-full aspect-video relative lg:rounded-md -z-10"
+                    src="https://player.twitch.tv/?channel=patopapao&amp;enableExtensions=false&amp;parent=twitch.tv&amp;quality=auto&amp;volume=1&amp;parent=https://keyd-watch.vercel.app/" type="text/html" allowFullScreen
                 >
                 </iframe>
             </div>
 
             {/* 3° tela */}
-            < div className=" w-full max-w-sm min-h-[500px] lg:min-h-0 lg:max-w-none col-start-10 col-span-3 row-start-4 row-span-7 lg:rounded-md overflow-y-auto ">
+            < div className={"w-full max-w-sm min-h-[500px] lg:min-h-0 lg:max-w-none col-start-10 col-span-3 row-start-4 row-span-7 lg:rounded-md overflow-y-auto" + swapScreen.third}>
                 {acompanhamento === 'twitchChat' && (
                     <iframe
                         className='w-full h-full lg:min-h-0'
@@ -180,7 +228,7 @@ export default function Live() {
                     ></iframe>
                 )}
                 {acompanhamento === 'comunidade' && (
-                          <DisqusComments />
+                    <DisqusComments />
 
                 )}
                 {acompanhamento === 'termo' && (
